dab63cdc14bc998b231adc96cec7ef1bd04da91d
Tetra <rogi.1107@live.com>
2019-03-14 11:18:55 +0100

    Add .js & .html index file for game

---
+++ BackgammonGame/bg-index.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+let con = mysql.createConnection({
+	host: "10.194.69.15",
+	user: "A2",
+	password: "08DeFYaH16z75hVx",
+	database: "A2"
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+con.connect(function(err){
+	if (err) throw err;
+	console.log('Connected!');
+});
+
+app.get('/',function(req,res) { //remplace le hello world par une page html
+  res.sendFile(__dirname+'/bg-index.html');
+});
+
+app.post('/signup', function (req, res) {
+   var postData  = req.body;
+   con.query('INSERT INTO players SET ?', postData, function (error, results, fields) {
+	  if (error) throw error;
+	  res.send(results);
+	});
+});
