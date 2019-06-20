71fbdfdd7a94c40bf0a5d79923192eb3c21e883c
Tetra <rogi.1107@live.com>
2019-06-20 11:18:54 +0200

    Implemented rooms on server side

--- bg2/server.js
+++ bg2/server.js
@@ -17,14 +17,13 @@ momentDurationFormatSetup(moment);
 const port = process.env.PORT || 3000;
 
 const app = express();
-
-const server = http.createServer(app);
-const io = socketIO(server);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session);
 
+const server = http.createServer(app);
+
+const io = socketIO(server);
 io.use(sharedsession(session, {
   autoSave: true
 }));
@@ -58,54 +57,12 @@ function GameData(p1_pos, p2_pos, p1Name, p2Name, p1Ready, p2Ready, dice1Value, 
   this.roomNo = roomNo;
 }
 
-let room1Data = new GameData( 
-  ['','','','','','','','','','','','','','','','','','','','','','','','','',''], 
-  ['','','','','','','','','','','','','','','','','','','','','','','','','',''], 
-  'Joueur 1', 
-  'Joueur 2', 
-  '', 
-  '', 
-  null, 
-  null, 
-  0, 
-  false, 
-  false,
-  1
-);
-
-let room2Data = new GameData( 
-  ['','','','','','','','','','','','','','','','','','','','','','','','','',''], 
-  ['','','','','','','','','','','','','','','','','','','','','','','','','',''], 
-  'Joueur 1', 
-  'Joueur 2', 
-  '', 
-  '', 
-  null, 
-  null, 
-  0, 
-  false, 
-  false,
-  2
-);
-
-let room3Data = new GameData( 
-  ['','','','','','','','','','','','','','','','','','','','','','','','','',''], 
-  ['','','','','','','','','','','','','','','','','','','','','','','','','',''], 
-  'Joueur 1', 
-  'Joueur 2', 
-  '', 
-  '', 
-  null, 
-  null, 
-  0, 
-  false, 
-  false,
-  3
-);
-
+let room1Data = new GameData(['','','','','','','','','','','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','','','','','','','','','','',''],'Joueur 1','Joueur 2','','',null,null,0,false,false,1);
+let room2Data = new GameData(['','','','','','','','','','','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','','','','','','','','','','',''],'Joueur 1','Joueur 2','','',null,null,0,false,false,2);
+let room3Data = new GameData(['','','','','','','','','','','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','','','','','','','','','','',''],'Joueur 1','Joueur 2','','',null,null,0,false,false,3);
 let rooms = [room1Data, room2Data, room3Data];
 
-
+/*
 let p1_pos = ['','','','','','','','','','','','','','','','','','','','','','','','','',''];
 let p2_pos = ['','','','','','','','','','','','','','','','','','','','','','','','','',''];
 let p1Name = 'Joueur 1';
@@ -117,6 +74,7 @@ let dice2Value = null;
 let playerTurn = 0;
 let gameIsRunning = false;
 let victory = false;
+*/
 
 //Where the action starts --> called when a user connects to the server
 io.on('connection', socket => {
@@ -125,16 +83,18 @@ io.on('connection', socket => {
   let playerNo = 0; //number of the player (1 or 2)
   let hasPlayed = false; //true if the user has played, otherwise false
   let room = null;
-  let roomData;
+  let roomData = null;
   
   console.log('\nUser connected')
 
+  //Called when user logs in
   socket.on("login", function(userdata) {
     console.log(userdata)
     socket.handshake.session.userdata = userdata;
     socket.handshake.session.save();
   });
 
+  //Called when user logs out
   socket.on("logout", function(userdata) {
     if (socket.handshake.session.userdata) {
       delete socket.handshake.session.userdata;
@@ -146,13 +106,8 @@ io.on('connection', socket => {
   socket.on('username', data => {
     username = data;
     socket.emit('loginStatus', true)
-    //On login, server sends the actual situation to the new-coming user
-    socket.emit('updatePos', [p1_pos,p2_pos])
-    socket.emit('updatePlayer1', [p1Name, p1Ready])
-    socket.emit('updatePlayer2', [p2Name, p2Ready])
-    socket.emit('diceValues', [dice1Value,dice2Value]);
 
-    //Check if the suer is already in the database
+    //Check if the user is already in the database
     con.query("SELECT name FROM players WHERE name=?", [username],function(err,rows){
       if(err) console.log("\n>>> [mysql error] :",err);
       if(!err){
@@ -172,101 +127,102 @@ io.on('connection', socket => {
 
   //Called when user enters a room
   socket.on('selectRoom', data => {
-    if(room != null) socket.leave(room)
     room = data
     socket.join(room)
     roomData = rooms[room-1]
     console.log(username + " joined the room number " + roomData.roomNo)
     io.to(room).emit('message', `${username} a rejoins le salon ${roomData.roomNo}`)
+    //On login, server sends the actual situation to the new-coming user
+    socket.emit('updatePos', [roomData.p1_pos,roomData.p2_pos])
+    socket.emit('updatePlayer1', [roomData.p1Name, roomData.p1Ready])
+    socket.emit('updatePlayer2', [roomData.p2Name, roomData.p2Ready])
+    socket.emit('diceValues', [roomData.dice1Value,roomData.dice2Value]);
+    socket.emit('canLeaveRom', true)
   });
 
   //Called when user chooses its "seat" (player1 or player2)
   socket.on('choosePlayer', data => {
     playerNo = data[2]
     if (playerNo == 1) {
-      p1Name = data[0];
-      p1Ready = data[1];
-      console.log('Player 1 :',p1Name,' status is ', p1Ready);
-      io.sockets.emit('updatePlayer1', [p1Name, p1Ready])
+      roomData.p1Name = data[0];
+      roomData.p1Ready = data[1];
+      console.log('Player 1 :',roomData.p1Name,' status is ', roomData.p1Ready);
+      io.to(room).emit('updatePlayer1', [roomData.p1Name, roomData.p1Ready])
     }
     else if (playerNo == 2) {
-      p2Name = data[0];
-      p2Ready = data[1];
-      console.log('Player 2 :',p2Name,' status is ', p2Ready);
-      io.sockets.emit('updatePlayer2', [p2Name, p2Ready])
+      roomData.p2Name = data[0];
+      roomData.p2Ready = data[1];
+      console.log('Player 2 :',roomData.p2Name,' status is ', roomData.p2Ready);
+      io.to(room).emit('updatePlayer2', [roomData.p2Name, roomData.p2Ready])
     }
     else console.log("Problem with the player") 
-    getRunningGames(function(rows1){
+    getRunningGames(roomData.p1Name, roomData.p2Name, function(rows1){
       if(rows1 != "no") {
-        io.sockets.emit('runningGame', true)
+        io.to(room).emit('runningGame', true)
       }
-      else io.sockets.emit('runningGame', false)
+      else io.to(room).emit('runningGame', false)
     });
     socket.emit('canLeave', true)
   });
 
-  //Called when user leavs its "seat"
-  socket.on('leave', data => {
-    idFromName(p1Name, function(id){
-      let idtempo_ = id
-      idFromName(p2Name, function(id){
-        let id2tempo_ = id
-          register(data, playerTurn, idtempo_, id2tempo_, function(ok){});
-      });
-    });
-    socket.emit('canLeave', false)
-  });
-
   //Called when user starts a new game
   socket.on('startNewGame', data => {    
-    io.sockets.emit('runningGame', false)
+    io.to(room).emit('runningGame', false)
 
-    gameIsRunning = true;
-    console.log("GameRunning" + gameIsRunning)
+    roomData.gameIsRunning = true;
+    console.log("GameRunning" + roomData.gameIsRunning)
 
     const p1_pos_init = [14,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; //Start position of p1 
     const p2_pos_init = [0,0,0,2,0,0,0,0,0,0,0,0,5,0,0,0,0,3,0,5,0,0,0,0,0,0]; //start position of p2
-    p1_pos = p1_pos_init;
-    p2_pos = p2_pos_init;
-    dice1Value = null;
-    dice2Value = null;
-    playerTurn = data;
+    roomData.p1_pos = p1_pos_init;
+    roomData.p2_pos = p2_pos_init;
+    roomData.dice1Value = null;
+    roomData.dice2Value = null;
+    roomData.playerTurn = data;
 
-    con.query("DELETE FROM running_games WHERE (player1=(SELECT id FROM players WHERE name=?) and player2=(SELECT id FROM players WHERE name=?))", [p1Name, p2Name, p1Name, p2Name], function(err){
+    con.query("DELETE FROM running_games WHERE (player1=(SELECT id FROM players WHERE name=?) and player2=(SELECT id FROM players WHERE name=?))", [roomData.p1Name, roomData.p2Name, roomData.p1Name, roomData.p2Name], function(err){
       if(err) console.log("\n>>> [mysql error] :", err);
     });
-    con.query("INSERT INTO running_games(pos_p1, pos_p2, points_p1, points_p2, dice1, dice2, player_turn, hub, player1, player2) VALUES(?, ?, 0, 0, ?, ?, ?, 0, (SELECT id FROM players WHERE name=?), (SELECT id FROM players WHERE name=?))", [JSON.stringify(p1_pos),JSON.stringify(p2_pos),dice1Value, dice2Value, playerTurn,p1Name, p2Name], function(err){
+    con.query("INSERT INTO running_games(pos_p1, pos_p2, points_p1, points_p2, dice1, dice2, player_turn, hub, player1, player2) VALUES(?, ?, 0, 0, ?, ?, ?, 0, (SELECT id FROM players WHERE name=?), (SELECT id FROM players WHERE name=?))", [JSON.stringify(roomData.p1_pos),JSON.stringify(roomData.p2_pos),roomData.dice1Value, roomData.dice2Value, roomData.playerTurn,roomData.p1Name, roomData.p2Name], function(err){
       if (err) console.log("\n>>> [mysql error] :", err);
     });
 
-    io.sockets.emit('updatePos', [p1_pos,p2_pos]);
-    io.sockets.emit('diceValues', [dice1Value,dice2Value]);
-    io.sockets.emit('nextTurn', playerTurn);
+    io.to(room).emit('updatePos', [roomData.p1_pos,roomData.p2_pos]);
+    io.to(room).emit('diceValues', [roomData.dice1Value,roomData.dice2Value]);
+    io.to(room).emit('nextTurn', roomData.playerTurn);
   });
 
   //Called when user continue running game
   socket.on('continueGame', () => {
-    io.sockets.emit('runningGame', false)
+    io.to(room).emit('runningGame', false)
 
-    gameIsRunning = true;
-    console.log("GameRunning" + gameIsRunning)
+    roomData.gameIsRunning = true;
+    console.log("GameRunning" + roomData.gameIsRunning)
 
-    getRunningGames(function(rows1){
+    getRunningGames(roomData.p1Name, roomData.p2Name, function(rows1){
       if(rows1 != "no") {
-        p1_pos = JSON.parse(rows1[0].pos_p1);
-        p2_pos = JSON.parse(rows1[0].pos_p2);
-        playerTurn = rows1[0].player_turn;
+        roomData.p1_pos = JSON.parse(rows1[0].pos_p1);
+        roomData.p2_pos = JSON.parse(rows1[0].pos_p2);
+        roomData.playerTurn = rows1[0].player_turn;
         points_p1 = 0;
         points_p2 = 0;
-        dice1Value = rows1[0].dice1;
-        dice2Value = rows1[0].dice2;
+        roomData.dice1Value = rows1[0].dice1;
+        roomData.dice2Value = rows1[0].dice2;
                
-        io.sockets.emit('updatePos', [p1_pos,p2_pos]);
-        io.sockets.emit('diceValues', [dice1Value,dice2Value]);
-        io.sockets.emit('nextTurn', playerTurn);
+        io.to(room).emit('updatePos', [roomData.p1_pos,roomData.p2_pos]);
+        io.to(room).emit('diceValues', [roomData.dice1Value,roomData.dice2Value]);
+        io.to(room).emit('nextTurn', roomData.playerTurn);
       }
     });
   });
+
+  //Called when the player throws the dices
+  socket.on('throwDice', () => {
+    roomData.dice1Value = throwDice();
+    roomData.dice2Value = throwDice();
+    io.to(room).emit('diceValues', [roomData.dice1Value,roomData.dice2Value] );
+  });
+
   // Called when the player wants to move a pawn
   socket.on('movePawn', data => {
     const index = data[0];
@@ -279,30 +235,30 @@ io.on('connection', socket => {
     playerNo : the player number (1 or 2)
     */
     
-    let newPos = move(playerNo, p1_pos, p2_pos, index, diceValue, socket);
+    let newPos = move(playerNo, roomData.p1_pos, roomData.p2_pos, index, diceValue, socket);
     
-    p1_pos = newPos.p1_pos;
-    p2_pos = newPos.p2_pos;
-    io.sockets.emit('updatePos', [p1_pos,p2_pos])
+    roomData.p1_pos = newPos.p1_pos;
+    roomData.p2_pos = newPos.p2_pos;
+    io.to(room).emit('updatePos', [roomData.p1_pos,roomData.p2_pos])
 
     hasPlayed = newPos.hasPlayed;
     if(hasPlayed) {
       if(diceNo == 1) {
-        dice1Value = null;
-        io.sockets.emit('diceValues', [dice1Value, dice2Value]);
+        roomData.dice1Value = null;
+        io.to(room).emit('diceValues', [roomData.dice1Value, roomData.dice2Value]);
       }
       else if(diceNo == 2) {
-        dice2Value = null;
-        io.sockets.emit('diceValues', [dice1Value, dice2Value]);
+        roomData.dice2Value = null;
+        io.to(room).emit('diceValues', [roomData.dice1Value, roomData.dice2Value]);
       }
       if(playedDice == 1) {
         if(playerNo == 1) {
-          playerTurn = 2
-          io.sockets.emit('nextTurn', playerTurn)
+          roomData.playerTurn = 2
+          io.to(room).emit('nextTurn', roomData.playerTurn)
         }
         else if(playerNo == 2) {
-          playerTurn = 1
-          io.sockets.emit('nextTurn', playerTurn)
+          roomData.playerTurn = 1
+          io.to(room).emit('nextTurn', roomData.playerTurn)
         }
         playedDice = 0
       }
@@ -311,8 +267,8 @@ io.on('connection', socket => {
       }
     }
     
-    victory = newPos.victory;
-    if(victory) {
+    roomData.victory = newPos.victory;
+    if(roomData.victory) {
       let id_
       let player1_
       let player2_
@@ -329,7 +285,7 @@ io.on('connection', socket => {
         score1_ = 0
         score2_ = 1
       }
-      getRunningGames(function(rows1){
+      getRunningGames(roomData.p1Name, roomData.p2Name, function(rows1){
         id_ = rows1[0].id
         player1_ = rows1[0].player1
         player2_ = rows1[0].player2
@@ -367,24 +323,17 @@ io.on('connection', socket => {
         con.query("DELETE FROM running_games WHERE id=?", [id_], function(err1, rows1){
           if(err1) console.log("\n>>> [mysql error] :", err1);
         });        
-        gameIsRunning = false;
-        dice1Value = null;
-        dice2Value = null;
-        io.sockets.emit('diceValues', [dice1Value,dice2Value])
-        playerTurn = -1;
-        io.sockets.emit('nextTurn', playerTurn)
+        roomData.gameIsRunning = false;
+        roomData.dice1Value = null;
+        roomData.dice2Value = null;
+        io.to(room).emit('diceValues', [roomData.dice1Value,roomData.dice2Value])
+        roomData.playerTurn = -1;
+        io.to(room).emit('nextTurn', roomData.playerTurn)
         //Sending data to players
-        io.sockets.emit('summary', [id_,p1Name,p2Name, playingTime_, score1_, score2_])
+        io.to(room).emit('summary', [id_,roomData.p1Name,roomData.p2Name, playingTime_, score1_, score2_])
       });
     }
     
-  });
-
-  //Called when the player throws the dices
-  socket.on('throwDice', () => {
-    dice1Value = throwDice();
-    dice2Value = throwDice();
-    io.sockets.emit('diceValues', [dice1Value,dice2Value] );
   });
 
   //Called when user clicks on statistics
@@ -399,21 +348,53 @@ io.on('connection', socket => {
     });
   })
 
-  socket.on('disconnect', () => {
-    if (playerNo != 0) {
-      idFromName(p1Name, function(id){
-        let idtempo_ = id
-        idFromName(p2Name, function(id){
-          let id2tempo_ = id
-          register(playerNo, playerTurn, idtempo_, id2tempo_, function(ok){});
-        });
+  //Called when user leavs its "seat"
+  socket.on('leave', data => {
+    idFromName(roomData.p1Name, function(id){
+      let idtempo_ = id
+      idFromName(roomData.p2Name, function(id){
+        let id2tempo_ = id
+          register(roomData, data, idtempo_, id2tempo_, function(ok){});
       });
+    });
+    socket.emit('canLeave', false)
+  });
+
+  //Called when user leavs the room
+  socket.on('leaveRoom', data => {
+    socket.leave(room);
+    room = null;
+    roomData = null;
+    idFromName(roomData.p1Name, function(id){
+      let idtempo_ = id
+      idFromName(roomData.p2Name, function(id){
+        let id2tempo_ = id
+          register(roomData, data, idtempo_, id2tempo_, function(ok){});
+      });
+    });
+    socket.emit('canLeaveRoom', false)
+  });
+
+  socket.on('disconnect', () => {
+    if(room != null) {
+      socket.leave(room);
+      room = null;
+      roomData = null;
+      if (playerNo != 0) {
+        idFromName(roomData.p1Name, function(id){
+          let idtempo_ = id
+          idFromName(roomData.p2Name, function(id){
+            let id2tempo_ = id
+            register(roomData, playerNo, idtempo_, id2tempo_, function(ok){});
+          });
+        });
+      }
     }
     console.log('User disconnected')
   });
 });
 
-server.listen(3000, "0.0.0.0", () => console.log(`Listening on port ${port}`));
+server.listen(port, "0.0.0.0", () => console.log(`Listening on port ${port}`));
 
 process.on('SIGTERM', () => {
   console.log('Closing http server.')
@@ -422,32 +403,32 @@ process.on('SIGTERM', () => {
   })
 })
 
-register = function(playerNo, playerTurn, idtempo_, id2tempo_, callback){
+register = function(roomData, playerNo, idtempo_, id2tempo_, callback){
   
-  if(gameIsRunning) {
-    con.query("UPDATE running_games SET pos_p1=?, pos_p2=?, points_p1=?, points_p2=?, dice1=?, dice2=?, player_turn=?, hub=? WHERE (player1=? and player2=?)", [JSON.stringify(p1_pos),JSON.stringify(p2_pos),0,0, dice1Value, dice2Value, playerTurn, 0, idtempo_, id2tempo_], function(err){
+  if(roomData.gameIsRunning) {
+    con.query("UPDATE running_games SET pos_p1=?, pos_p2=?, points_p1=?, points_p2=?, dice1=?, dice2=?, player_turn=?, hub=? WHERE (player1=? and player2=?)", [JSON.stringify(roomData.p1_pos),JSON.stringify(roomData.p2_pos),0,0, roomData.dice1Value, roomData.dice2Value, roomData.playerTurn, 0, idtempo_, id2tempo_], function(err){
       if (err) console.log("\n>>> [mysql error] :", err);
     });
     console.log("Database updated");
-    gameIsRunning = false;
+    roomData.gameIsRunning = false;
   }
 
-  dice1Value = null;
-  dice2Value = null;
-  io.sockets.emit('diceValues', [dice1Value,dice2Value])
-  playerTurn = -1;
-  io.sockets.emit('nextTurn', playerTurn)
+  roomData.dice1Value = null;
+  roomData.dice2Value = null;
+  io.to(roomData.roomNo).emit('diceValues', [roomData.dice1Value,roomData.dice2Value])
+  roomData.playerTurn = -1;
+  io.to(roomData.roomNo).emit('nextTurn', roomData.playerTurn)
   
   if (playerNo == 1) {
-    p1Name = 'Joueur 1';
-    p1Ready = false;
-    io.sockets.emit('updatePlayer1', [p1Name, p1Ready]);
+    roomData.p1Name = 'Joueur 1';
+    roomData.p1Ready = false;
+    io.to(roomData.roomNo).emit('updatePlayer1', [roomData.p1Name, roomData.p1Ready]);
     console.log("Player 1 left");
   }
   else if (playerNo == 2) {   
-    p2Name = 'Joueur 2';
-    p2Ready = false;
-    io.sockets.emit('updatePlayer2', [p2Name, p2Ready]);
+    roomData.p2Name = 'Joueur 2';
+    roomData.p2Ready = false;
+    io.to(roomData.roomNo).emit('updatePlayer2', [roomData.p2Name, roomData.p2Ready]);
     console.log("Player 2 left");
   }
   else console.log("Problem with the player");
@@ -455,7 +436,7 @@ register = function(playerNo, playerTurn, idtempo_, id2tempo_, callback){
   callback("ok");
 }
 
-getRunningGames = function(callback) {
+getRunningGames = function(p1Name, p2Name, callback) {
   idFromName(p1Name, function(id){
     let idtempo_ = id
     idFromName(p2Name, function(id){
@@ -494,6 +475,7 @@ function move(player, newP1_pos, newP2_pos, position, diceValue, socket){
   let pawnSum = 0;
   let canPlay = false;
   let hasPlayed = false;
+  let victory = false;
 
   /*
 	if player1 : player = 1, if player2 : player = 2
